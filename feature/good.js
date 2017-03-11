6ae8016730ede9159a9e9e5cacc3fb3ca506a862
chenzj <chenzjchina@gmail.com>
2017-03-11 21:21:57 +0800

    small tips

--- feature/good.js
+++ feature/good.js
@@ -1,2 +1,14 @@
 This is good js hah
 stash
+git stash
+git stash apply
+git log --oneline --decorate -10 --graph
+
+git branch
+git checkout master
+git merge
+git add .
+git commit
+git am -m ''
+git reset
+git revert
